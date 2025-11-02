2a916b605ec5abfec60983e39dd5ca615e3dbfcf
MathZeke11 <joshua.e.wedge@outlook.com>
2025-11-02 14:55:56 +0000

    game over detection

--- Project-File/src/main.js
+++ Project-File/src/main.js
@@ -2,10 +2,11 @@ import * as THREE from "three";
 import { Renderer } from "./components/Renderer";
 import { Camera } from "./components/Camera";
 import { DirectionalLight } from "./components/DirectionalLight";
-import { player } from "./components/Player";
+import { player, initializePlayer } from "./components/Player";
 import { map, initializeMap } from "./components/Map";
 import { animateVehicles } from "./animateVehicles";
 import { animatePlayer } from "./animatePlayer";
+import { hitTest } from "./hitTest";
 import "./style.css";
 import "./collectUserInput";
 
@@ -23,10 +24,22 @@ player.add(dirLight);
 const camera = Camera();
 player.add(camera);
 
+const scoreDOM = document.getElementById("score");
+const resultDOM = document.getElementById("result-container");
+
 initializeGame();
 
+document
+  .querySelector("#retry")
+  ?.addEventListener("click", initializeGame);
+
 function initializeGame() {
+  initializePlayer();
   initializeMap();
+
+  // Initialize UI
+  if (scoreDOM) scoreDOM.innerText = "0";
+  if (resultDOM) resultDOM.style.visibility = "hidden";
 }
 
 const renderer = Renderer();
@@ -35,6 +48,7 @@ renderer.setAnimationLoop(animate);
 function animate() {
   animateVehicles();
   animatePlayer();
+  hitTest();
 
   renderer.render(scene, camera);
 }
