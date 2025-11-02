c5a88fc71387ee96acaf9729b70f9e9097db64d6
MathZeke11 <joshua.e.wedge@outlook.com>
2025-11-02 06:40:21 -0600

    shadows

--- Project-File/src/components/Tree.js
+++ Project-File/src/components/Tree.js
@@ -23,6 +23,8 @@ export function Tree(tileIndex, height) {
     })
   );
   crown.position.z = height / 2 + 20;
+  crown.castShadow = true;
+  crown.receiveShadow = true;
   tree.add(crown);
 
   return tree;
